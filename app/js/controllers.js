01f200e94f1bbf4abcea721e107421ee06e62fe5
mapugina <mapugina@gmail.com>
2014-05-08 12:25:43 -0600

    Load Defaults working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,21 +3,32 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('PlayerAdditionCtrl', ['$scope', function($scope) {
+  .controller('PlayerAdditionCtrl', function($scope, $http) {
 		$scope.players = [];
-		
+		$scope.playeralert = "";
+		$scope.alertclass = "alert";
 		//add a player
 		$scope.addPlayer = function()
+		{	
+			addToPlayers($scope.playername, $scope.playercolor);	
+		}
+		
+		$scope.loadDefaults = function()
+		{
+			$http.get('defaults.json').success(function(array)
+			{
+				$scope.players = array;	
+			});
+		}
+		
+		function addToPlayers(playername, playercolor)
 		{
 			$scope.players.push(
 				{
-					name: $scope.playername, 
-					color: $scope.playercolor
+					name: playername, 
+					color: playercolor
 				});
+				$scope.alertstyle = "alert-success";
+				$scope.playeralert = "Player " + $scope.playername + " added.";
 		}
-		
-		$scope.styleColor = function (player)
-		{
-			return { color: player.color }
-		}
-  }]);
+  });
