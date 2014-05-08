43a3f4e06a56b4a9005b0e9bada670dd94ab821c
mapugina <mapugina@gmail.com>
2014-05-08 14:29:36 -0600

    Seperated out player entry from index

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,7 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/playerentry', {templateUrl: 'partials/PlayerEntry.html', controller: 'PlayerAdditionCtrl'});
+  $routeProvider.when('/gameplay', {templateUrl: 'partials/gameplay.html', controller: 'PlayerAdditionCtrl'});
+  $routeProvider.otherwise({redirectTo: '/playerentry'});
 }]);
