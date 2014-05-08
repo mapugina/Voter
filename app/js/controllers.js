e418f8219179968bdf6f3c93c1fbd91f7a2afdcc
mapugina <mapugina@gmail.com>
2014-05-08 09:47:46 -0600

    Initial commit. Player list adding working

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,21 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
+  .controller('PlayerAdditionCtrl', ['$scope', function($scope) {
+		$scope.players = [];
+		
+		//add a player
+		$scope.addPlayer = function()
+		{
+			$scope.players.push(
+				{
+					name: $scope.playername, 
+					color: $scope.playercolor
+				});
+		}
+		
+		$scope.styleColor = function (player)
+		{
+			return { color: player.color }
+		}
   }]);
